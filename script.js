b2c72c26afa0f54c17316ee049a31d3d0cff16a3
A.Yahya <130736722+AmanCharlton@users.noreply.github.com>
2024-05-29 21:24:04 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let text = document.lastModified; document.getElementById("lastupdate").innerHTML = text;
+
+function message1(){
+    alert('You Will Be Redirected To Email Sender Software')
+}
+
+function message2(){
+    alert('The Form Had Been Successfully Reset')
+}
+
+function message3(){
+    alert('The Score Mark Will Be Submitted Momentarily')
+}
